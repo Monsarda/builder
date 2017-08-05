1f386e7fb96d78381bc2a874520840f1a36d7b7c
Monsarda <monsarda@yandex.ru>
2017-08-05 17:22:26 +0300

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,24 @@
 
 let gulp = require('gulp'),
 	less = require('gulp-less'),
-	sourcemaps = require('gulp-sourcemaps');
+	sourcemaps = require('gulp-sourcemaps'),
+	autoprefixer = require('gulp-autoprefixer'),
+	concat = require('gulp-concat');
 
-gulp.task('less', function(){
+gulp.task('css-dev', function(){
 	gulp.src('assets/less/**/*.less')
 		.pipe(sourcemaps.init())
-			.pipe(less())
-		.pipe(sourcemaps.write('./'))
+		.pipe(less())
+		.pipe(autoprefixer({
+            browsers: ['last 25 versions'],
+            cascade: true
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(sourcemaps.write('./'))
 	.pipe(gulp.dest('public/css'));
 });
 
 gulp.task('watch', function(){
-  	gulp.watch('assets/less/**/*.less', ['less']);
+  	gulp.watch('assets/less/index.less', ['css-dev']);
 });
