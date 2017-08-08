028c8ddc455a5afdbff70aa808ee319ef80858b7
Monsarda <monsarda@yandex.ru>
2017-08-08 11:11:59 +0300

    stable less compilation

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,16 @@ let gulp = require('gulp'),
 	less = require('gulp-less'),
 	sourcemaps = require('gulp-sourcemaps'),
 	autoprefixer = require('gulp-autoprefixer'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+    gutil = require('gulp-util'),
+    chalk = require('chalk');
 
 gulp.task('css-dev', function(){
 	gulp.src('assets/less/**/*.less')
 		.pipe(sourcemaps.init())
-		.pipe(less())
+		.pipe(less()).on('error', function(error) {
+            gutil.log('Error Less: ', '\'' + chalk.red(error.message));
+        })
 		.pipe(autoprefixer({
             browsers: ['last 25 versions'],
             cascade: true
@@ -17,7 +21,9 @@ gulp.task('css-dev', function(){
         .pipe(sourcemaps.init())
         .pipe(concat('style.css'))
         .pipe(sourcemaps.write('./'))
-	.pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest('public/css')).on('end', function() {
+        gutil.log(chalk.green('Success Less Compilation'));
+    });
 });
 
 gulp.task('watch', function(){
