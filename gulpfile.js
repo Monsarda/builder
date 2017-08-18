ea254458f7054bb94c531a54d69763cfe3384a15
Monsarda <monsarda@yandex.ru>
2017-08-18 07:31:54 +0300

    Update Tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,6 @@ let gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
     // Конкатенация файлов
 	concat = require('gulp-concat'),
-    // Инклуд
-    rigger = require('gulp-rigger'),
     // Watcher custom
     watch = require('gulp-watch'),
     // Ловим ошибки
@@ -23,32 +21,53 @@ let gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     // HTML Validator
     htmlhint = require('gulp-htmlhint'),
-    cleanCss = require('gulp-clean-css');
+    // Min CSS
+    cleanCss = require('gulp-clean-css'),
+    // Web Server
+    connect = require('gulp-connect');
+
 
 let path = {
 
     build: {
         html: 'build/',
-        css: 'build/css/',
+        style: 'build/css/',
         js: 'build/js/',
         img: 'build/img/',
-        fonts: 'build/fonts/'
+        fonts: 'build/fonts/',
+        files: 'build/files/'
     },
     src: {
         html: 'src/html/*.html',
         style: 'src/less/index.less',
         js: 'src/js/*.js',
         img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        fonts: 'src/fonts/**/*.*',
+        files: 'src/files/**/*.*'
     },
     watch: {
         html: 'src/html/**/*.html',
         style: 'src/less/**/*.less',
         js: 'src/js/*.js',
         img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        fonts: 'src/fonts/**/*.*',
+        files: 'src/files/**/*.*'
     }
 }
+
+let config = {
+
+    server: {
+        root: 'build',
+        port: 8000,
+        livereload: true
+    }
+
+}
+
+gulp.task('server:build', function () {
+    connect.server(config.server);
+});
 
 // BUILD //
 gulp.task('style:build', function () {
@@ -63,8 +82,9 @@ gulp.task('style:build', function () {
         .pipe(concat('style.css'))
         .pipe(cleanCss({compatibility: 'ie8'}))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(path.build.css))
-        .pipe(notify('Less Compilation Done'));
+        .pipe(gulp.dest(path.build.style))
+        .pipe(notify('Less Compilation Done'))
+        .pipe(connect.reload());
     });
 });
 
@@ -78,7 +98,8 @@ gulp.task('js:build', function () {
         .pipe(gulp.dest(path.build.js+'paths'))
         .pipe(concat('script.js'))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(path.build.js));
+        .pipe(gulp.dest(path.build.js))
+        .pipe(connect.reload());
         
     });
 });
@@ -94,7 +115,8 @@ gulp.task('image:build', function () {
             svgoPlugins: [{removeViewBox: true}]
         }))
         .pipe(gulp.dest(path.build.img))
-        .pipe(notify('Images Finish'));
+        .pipe(notify('Images Finish'))
+        .pipe(connect.reload());
         
     });
 });
@@ -103,29 +125,46 @@ gulp.task('html:build', function () {
     return watch(path.watch.html, function(){
         gulp.src(path.src.html)
         .pipe(plumber())
-        .pipe(htmlhint())
-        .pipe(htmlhint.failReporter())
+        .pipe(htmlhint({
+            "attr-value-not-empty": true,
+            "attr-no-duplication": true,
+            "tag-pair": true,
+            "id-unique": true,
+            "doctype-html5": true,
+        }))
+        .pipe(htmlhint.reporter())
         .pipe(gulp.dest(path.build.html))
-        .pipe(notify('Html Finish'));
+        .pipe(notify('Html Finish'))
+        .pipe(connect.reload());
         
     });
 });
 
 gulp.task('fonts:build', function () {
-    return watch(path.watch.html, function(){
+    return watch(path.watch.fonts, function(){
         gulp.src(path.src.fonts)
         .pipe(gulp.dest(path.build.fonts))
         .pipe(notify('Fonts Finish'))
-        
+        .pipe(connect.reload());
     });
 });
 
+gulp.task('files:build', function () {
+    return watch(path.watch.files, function(){
+        gulp.src(path.src.files)
+        .pipe(gulp.dest(path.build.files))
+        .pipe(notify('Fonts Finish'))
+        .pipe(connect.reload());
+    });
+});
 
 gulp.task('build', [
+    'server:build',
     'html:build',
     'js:build',
     'style:build',
     'fonts:build',
-    'image:build'
+    'image:build',
+    'files:build'
 ]);
 
